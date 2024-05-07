ebad6c4c9724c47019ed8e1663f74d48b39308e1
veronica-7 <vettonica18@gmail.com>
2024-05-07 07:51:21 -0500

    Atributos clase persona

--- Delivery1/src/main/java/Clases/Persona.java
+++ Delivery1/src/main/java/Clases/Persona.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
 /**
@@ -9,5 +6,55 @@ package Clases;
  * @author Usuario
  */
 public class Persona {
-    
+    private String cedula;
+    private String apellidos;
+    private String nombres;
+    private String mail;
+
+    public Persona() {
+    }
+
+    public Persona(String cedula, String apellidos, String nombres, String mail) {
+        this.cedula = cedula;
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.mail = mail;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "cedula=" + cedula + ", apellidos=" + apellidos + ", nombres=" + nombres + ", mail=" + mail + '}';
+    }
 }
