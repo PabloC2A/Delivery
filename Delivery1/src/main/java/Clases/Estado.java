f87955622afbaf3d791c8328fead7512b34e1d6e
Black Code <pabloandrescriolloalvarez@gmail.com>
2024-05-07 08:17:33 -0500

    Creacion Clase Estado

---
+++ Delivery1/src/main/java/Clases/Estado.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Estado {
+    private int tipo;
+    private String estado;
+    private Date fecha;
+    private String observacion;
+
+    public Estado() {
+    }
+
+    public Estado(int tipo, String estado, Date fecha, String observacion) {
+        this.tipo = tipo;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.observacion = observacion;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Estado{" + "tipo=" + tipo + ", estado=" + estado + ", fecha=" + fecha + ", observacion=" + observacion + '}';
+    }
+}
