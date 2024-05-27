6c4a7163095ef6f3fdf17432b552862576e16190
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2024-05-26 22:47:29 -0500

    metodos db

--- Delivery1/src/main/java/Database/DBEntrega.java
+++ Delivery1/src/main/java/Database/DBEntrega.java
@@ -14,12 +14,36 @@ import java.sql.SQLException;
  * @author Pablo
  */
 public class DBEntrega {
-        Conexion BLcon = new Conexion();
+
+    Conexion BLcon = new Conexion();
 
     public ResultSet obtenerEntregas(Entrega objEntrega) throws SQLException {
         String consulta
                 = "SELECT FROM entrega WHERE *";
         PreparedStatement ps = BLcon.getConnection().prepareStatement(consulta);
+        return ps.executeQuery();
+    }
+
+    public ResultSet obtenerEntregasPorCliente(Entrega objEntrega) throws SQLException {
+        String consulta
+                = """
+                  SELECT *
+                  FROM delivery.entrega e
+                  INNER JOIN delivery.cliente c ON e.clienteCedula = c.cedulaCliente
+                  WHERE c.cedulaCliente = '?';""";
+        PreparedStatement ps = BLcon.getConnection().prepareStatement(consulta);
+        ps.setString(1, objEntrega.getIdCliente());
+        return ps.executeQuery();
+    }
+    
+    public ResultSet obtenerEntregasPorCodPaquete(Entrega objEntrega) throws SQLException {
+        String consulta
+                = """
+                  SELECT *
+                  FROM delivery.entrega
+                  WHERE idPaquete = ?;""";
+        PreparedStatement ps = BLcon.getConnection().prepareStatement(consulta);
+        ps.setInt(1, objEntrega.getIdPaquete());
         return ps.executeQuery();
     }
 }
