f18325e768474ccd1392bf9b8d6d94c9321d3809
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2024-05-26 19:55:52 -0500

    Creando conexion

--- Delivery1/src/main/java/Database/Conexion.java
+++ Delivery1/src/main/java/Database/Conexion.java
@@ -1,14 +1,27 @@
 package Database;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @author Pablo
- */
 public class Conexion {
-    
+    private Connection con;
+    private static final String URL = "jdbc:mysql://localhost:3306/delivery";
+    private static final String USER = "root";
+    private static final String PASSWORD = "CgXPqrFYAdaMLAP8dJiV";
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driver);
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+
+    public Connection AbrirConexion() throws ClassNotFoundException, SQLException {
+        con = getConnection();
+        return con;
+    }
+
+    public void CerrarConexion() throws SQLException {
+        con.close();
+    }
 }
